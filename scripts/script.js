c5972060673f0eb149a7281a85082b3a2ebadbe8
Diana <dianastanciu13@yahoo.co.uk>
2018-01-10 12:46:49 +0200

    burger x

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $(".burger-container").on("click", function() {
+    $(".burger").toggleClass("burger-x");
     $(".hidden-menu").toggleClass("hide-hidden-menu").toggleClass("show-hidden-menu");
     $("body").toggleClass("hide-scroll-body");
   });
